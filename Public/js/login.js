459b74326c8f940272d83953448f5e839c384559
DengZuoheng <dengzuoheng@gmail.com>
2014-08-28 11:08:45 +0800

    用jquery重构login.js代码
    1.不用对比不一样了,几乎都改动过;
    2.做过基本测试,但是没有实际的登陆测试,请后台人员看到后发布到SAE 做登陆测试,有bug请联系邓作恒

--- Public/js/login.js
+++ Public/js/login.js
@@ -1,181 +1,114 @@
 /*author:dengzuoheng form KSC 2014*/
+try{
+    $(document).ready(function(){
+        $("#login_button").click(function(){
+            var strUserName = $("#user_login_name").val();
+            var strUserPW = $("#user_login_pw").val();
+            function IsValid( oField ) 
+            { 
+                console.log("IsValid");
+                re= /select|update|delete|exec|count|'|"|=|;|>|<|%/i; 
+            
+                if ( re.test(oField.toLowerCase()) ) 
+                { 
+                    return false;
+                }
+                return true;
+            } 
+            
+            function validUserName(str)
+            {
+                console.log("validUserName");
+                re = /^\d{10}$/;
+                if(re.test(str) && IsValid(str) &&str.length==10)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            function validUserPW(str)
+            {
+                console.log("validUserPW");
+                return IsValid(str);
+            }
+            //验证码事件处理
+            if(""==$('#vertication_code').val())
+            {
+                $("#login_error").html("*验证码不能为空!");
+                $("#vertication_code").css("border-color","red");
+                return false;
+            }
+            else
+            {
+                try{
+                    checknumber=$('#vertication_code').val();
+                    var obj;
+                    $.ajax({
+                        type:"POST",
+                        url:URL+"/vertication",
+                        data:{'checknumber':checknumber},
+                        dataType:"json",
+                        async:false,
+                        success:function(result){
+                            obj=result;			
+                        },
+                    });
+                    if(obj.checked==0)
+                    {
+                        $("#login_error").html("*验证码错误!");
+                        $("#vertication_code").css("border-color","red").val("");
+                        $('#vertication_img').attr('src',URL+'/vertication');
+                        return false;
+                    }
+                }
+                catch(err){
+                    $("#login_error").html("*验证码发送错误,请刷新页面重新登录");
+                    return false;
+                }
+            }
+            if(!validUserName(strUserName))
+            {
+                $("#login_error").html("*非法用户名!");
+                $("#user_login_name").css("border-color","red").val("");
+                return false;
+            }
+            else if(!validUserPW(strUserPW))
+            {
+                $("#login_error").html("*密码或用户名错误！");
+                $("#user_login_pw").css("border-color","red").val("");
+                return false;
+            }
+            else if(""==strUserName)
+            {
+                $("#login_error").html("*用户名不能为空！");
+                $("#user_login_name").css("border-color","red");
+                return false;
+            }
+            else 
+            {
+                $("form #user_login_pw").val(hex_md5($("form #user_login_pw").val()));
+                return true;
+            }
+        });
+        $("#cancel_button").click(function(){history.go(-1)});
+        $("#user_login_name,#user_login_pw,#vertication_code").focus(function(){
+                $(this).css("border-color","black");
+            }).blur(function(){
+                $(this).css("border-color","#999999");
+                });
+        
+        //验证码切换图片
+        $('#vertication_img').attr('src',URL+'/vertication').click(function(){
+            $('#vertication_img').attr('src',URL+'/vertication');	
+        });
 
-window.onload=initLogin;
-
-function initLogin()
-{
-	
-	document.getElementById("login_button").onclick=function()
-	{
-
-		var objForm = document.forms["login_info"];
-		var strUserName = objForm.elements["user_login_name"].value;
-		var strUserPW = objForm.elements["user_login_pw"].value;
-		function IsValid( oField ) 
-		{ 
-			re= /select|update|delete|exec|count|'|"|=|;|>|<|%/i; 
-		
-			if ( re.test(oField.toLowerCase()) ) 
-			{ 
-				return false;
-			}
-			return true;
-		} 
-		
-		function validUsername(str)
-		{
-			re = /^\d{10}$/;
-			if(re.test(str) && IsValid(str) &&str.length==10)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		
-		function validUserPW(str)
-		{
-			return IsValid(str);
-		}
-		//验证码事件处理
-		if(""==$('#vertication_code').val())
-		{
-			document.getElementById("login_error").innerHTML="*验证码不能为空!";
-			document.getElementById("vertication_code").style.borderColor="red";
-			return false;
-		}
-		else
-		{
-			checknumber=$('#vertication_code').val();
-			var obj;
-			$.ajax({
-				type:"POST",
-				url:URL+"/vertication",
-				data:{'checknumber':checknumber},
-				dataType:"json",
-				async:false,
-				success:function(result){
-					obj=result;			
-				},
-			});
-			if(obj.checked==0)
-			{
-				document.getElementById("login_error").innerHTML="*验证码错误!";
-				document.getElementById("vertication_code").style.borderColor="red";
-				objForm.elements["vertication_code"].value="";
-				//alert("验证码错误");
-				$('#vertication_code').val("")
-				$('#vertication_img').attr('src',URL+'/vertication');
-				return false;
-			}
-		}
-		if(!validUsername(strUserName))
-		{
-			document.getElementById("login_error").innerHTML="*非法用户名!";
-			document.getElementById("user_login_name").style.borderColor="red";
-			objForm.elements["user_login_name"].value="";
-			return false;
-		}
-		else if(!validUserPW(strUserPW))
-		{
-			document.getElementById("login_error").innerHTML="*密码或用户名错误！";
-			document.getElementById("user_login_pw").style.borderColor="red";
-			objForm.elements["user_login_pw"].value="";
-			return false;
-		}
-		else if(""==strUserName)
-		{
-			document.getElementById("login_error").innerHTML="*用户名不能为空！";
-			document.getElementById("user_login_name").style.borderColor="red";
-			return false;
-		}
-		else 
-		{
-			/*
-			strError=postNameandPW(strUserName,strUserPW);
-			if(strError=="")
-			{	
-				if(document.getElementById("keep_login").checked=true)
-				{
-					writeCookie(strUserName,strUserPW);
-				}
-				//返回上一页并刷新
-				history.go(-1);
-				location.reload();
-			}
-			else
-			{
-				document.getElementById("login_error").innerHTML=strError;
-				document.getElementById("user_login_name").style.borderColor="red";
-				document.getElementById("user_login_pw").style.borderColor="red";
-			}
-			*/
-			var strPW=objForm.elements["user_login_pw"].value;
-			objForm.elements["user_login_pw"].value=hex_md5(strPW);
-			return true;
-		}
-	}
-	document.getElementById("cancel_button").onclick=function()
-	{
-		history.go(-1);
-	}
-	document.getElementById("user_login_name").onfocus=function()
-	{
-		document.getElementById("user_login_name").style.borderColor="black";
-	}
-	document.getElementById("user_login_name").onblur=function()
-	{
-		document.getElementById("user_login_name").style.borderColor="#999999";
-	}
-	document.getElementById("user_login_pw").onblur=function()
-	{
-		document.getElementById("user_login_pw").style.borderColor="#999999";
-	}
-	document.getElementById("user_login_pw").onfocus=function()
-	{
-		document.getElementById("user_login_pw").style.borderColor="black";
-	}
-	document.getElementById("user_login_pw").onfocus=function()
-	{
-		document.getElementById("user_login_pw").style.borderColor="black";
-	}
-	document.getElementById("vertication_code").onfocus=function()
-	{
-		document.getElementById("vertication_code").style.borderColor="black";
-	}
-	document.getElementById("vertication_code").onblur=function()
-	{
-		document.getElementById("vertication_code").style.borderColor="#999999";
-	}
-	//验证码切换图片
-	$('#vertication_img').attr('src',URL+'/vertication');
-	$('#vertication_img').click(function(){
-		$('#vertication_img').attr('src',URL+'/vertication');
-		
-	});
-
-}
-
-//发送用户名和密码
-function postNameandPW(strUserName,strUserPW)
-{
-	//alert(strUserName);
-	//alert(strUserPW);
-	//发送密码和用户名，成功返回空字符串，失败返回失败原因
-		
-	var jsonPW=
-	{
-		"username":strUserName,
-		"password":strUserPW,
-	};
-	
-	return "";//返回失败原因
-}
-
-//写入cookie
-function writeCookie(strUserName,strUserPW)
-{
-	return 0;
+    });
+}catch(err){
+    //出了奇怪的错误就把登录按钮无效化,并要求通知管理员
+    document.getElementById("login_error").innerHTML="*"+err+"\t请联系系统管理员";
+    document.getElementById("login_button").onclick=function(){return false;};
 }
