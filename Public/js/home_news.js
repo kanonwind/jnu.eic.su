32a69ffa41146d0dad83db7ff8a0d9b1ba38e815
DengZuoheng <dengzuoheng@gmail.com>
2014-10-01 16:18:24 +0800

    修改搜索框,获取焦点是高亮问题

--- Public/js/home_news.js
+++ Public/js/home_news.js
@@ -172,7 +172,7 @@ function NewsInit()
 		
 		//return;
 		document.getElementById("slide_news").innerHTML=strInnerHTML;      
-		search();
+		
 		objLeftPic = document.getElementById("leftPic");
 		objMidPic = document.getElementById("midPic");
 		objRightPic = document.getElementById("rightPic");
@@ -248,6 +248,20 @@ function NewsInit()
 		console.log(err);
 		console.log("新闻加载失败");
 	}
+    (function search()
+    {
+        console.log("search");
+        $("#query").focus(function(){
+            $("#query,#search_bar").css("background","white");
+            $("#searchsubmit").css("background","#52a3ed").mouseover(function(){$(this).css("background","#448aca");}).mouseout(function(){$(this).css("background","#52a3ed");});
+            console.log("focus");
+        }).blur(function(){
+            $("#query,#search_bar,#searchsubmit").css("background","white");
+            $("#searchsubmit").mouseover(function(){$(this).css("background","white");}).mouseout(function(){$(this).css("background","white");});
+            console.log("blur");
+        });
+    })();
+
 }
 
 //自动变换新闻
--- Public/js/searchbutton.js
+++ Public/js/searchbutton.js
@@ -1,37 +1,14 @@
-function search()
+
+(function search()
 {
-	document.getElementById("query").onfocus = function()
-	{
-		document.getElementById("query").style.background="white";
-		document.getElementById("search_bar").style.background="white";
-		
-		var obj=document.getElementById("searchsubmit");
-		obj.style.background="#52a3ed";
-		obj.onmouseover = function()
-		{
-			obj.style.background="#448aca";
-		}
-		obj.onmouseout = function()
-		{
-			obj.style.background="#52a3ed";
-		}
-		
-	}
-	document.getElementById("query").onblur = function()
-	{
-		var strColor="white";
-		document.getElementById("query").style.background= strColor;
-		document.getElementById("search_bar").style.background= strColor;
-		document.getElementById("searchsubmit").style.background= strColor;
-		var obj=document.getElementById("searchsubmit");
-		obj.style.background= strColor;
-		obj.onmouseover = function()
-		{
-			obj.style.background= strColor;
-		}
-		obj.onmouseout = function()
-		{
-			obj.style.background= strColor;
-		}
-	}
-}
+    console.log("search");
+    $("#query").focus(function(){
+        $("#query,#search_bar").css("background","white");
+        $("#searchsubmit").css("background","#52a3ed").mouseover(function(){$(this).css("background","#448aca");}).mouseout(function(){$(this).css("background","#52a3ed");});
+        console.log("focus");
+    }).blur(function(){
+        $("#query,#search_bar,#searchsubmit").css("background","white");
+        $("#searchsubmit").mouseover(function(){$(this).css("background","white");}).mouseout(function(){$(this).css("background","white");});
+        console.log("blur");
+    });
+})();
