d04c3c529f216573bc1612007838c4b1b415f547
Chenrenzhan <crz20130413@gmail.com>
2014-10-10 20:10:44 +0800

    修改“KSC联盟”为“JDC”
    修改“KSC联盟”为“JDC”

--- Public/js/userCenter.js
+++ Public/js/userCenter.js
@@ -1268,7 +1268,7 @@ function SortPersonalDataObj()
 	for(var iCount = 0; iCount < arrObjPersonalData.length; ++iCount)
 	{
 		var objTemp = arrObjPersonalData[iCount];
-		switch(objTemp.depart)
+		switch(arrDepartName[objTemp.depart])
 		{
 			case "主席团":
 				arrObjPR.push(objTemp);
@@ -1279,7 +1279,7 @@ function SortPersonalDataObj()
 			case "人力资源部":
 				arrObjHR.push(objTemp);
 				break;
-			case "KSC联盟":
+			case "JDC":
 				arrObjKSC.push(objTemp);
 				break;
 			case "信息编辑部":
@@ -1432,7 +1432,7 @@ function ShowContactsBooks()
 			+ "</tfoot>\n";
 	
 	var strTBody = "";
-	var arrDepart = new Array("主席团","秘书处","人力资源部","KSC联盟","信息编辑部",
+	var arrDepart = new Array("主席团","秘书处","人力资源部","JDC","信息编辑部",
 			"组织部","宣传部","学术部","公关部","体育部","文娱部","心理服务部");
             
     function getBirthStr(pObj)
@@ -1458,7 +1458,7 @@ function ShowContactsBooks()
 		if(arrObjSortPD[iCount].length > 0)
 		{
             var strDepart = "<tr class=\"first_line\">\n"
-						+ "<th rowspan=" + iLength + " scope=\"row\">" + arrObjSortPD[iCount][0].depart + "</th>\n"
+						+ "<th rowspan=" + iLength + " scope=\"row\">" + arrDepartName[arrObjSortPD[iCount][0].depart] + "</th>\n"
 						+ "<td>" + arrObjSortPD[iCount][0].post + "</td>\n"
 							+ "<td>" + arrObjSortPD[iCount][0].name + "</td>\n"
 							+ "<td>" + arrObjSortPD[iCount][0].QQNum + "</td>\n"
