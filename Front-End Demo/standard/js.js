f88db1e74c43fa42f9192bbdd02989db5cd0004d
DengZuoheng <dengzuoheng@gmail.com>
2014-11-12 23:33:20 +0800

    删除front-end demo,更新readme

--- Front-End Demo/standard/js.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    setInterval(function(){
-        console.log("scrollTop:"+$("#ccc").scrollTop()+">>>offsettop:"+$("#ccc").offset().top);
-        },1000)}
-        );
